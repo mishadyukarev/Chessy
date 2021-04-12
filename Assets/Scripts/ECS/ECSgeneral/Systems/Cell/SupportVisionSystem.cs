447fae573b31cd8467949d8382dc0b22dbc9ac39
Misha Dyukarev <misha21022001@mail.ru>
2021-04-12 16:48:06 +0300

    Update SupportVisionSystem.cs

--- Assets/Scripts/ECS/ECSgeneral/Systems/Cell/SupportVisionSystem.cs
+++ Assets/Scripts/ECS/ECSgeneral/Systems/Cell/SupportVisionSystem.cs
@@ -4,7 +4,7 @@ using static Main;
 
 public partial class SupportVisionSystem : CellReductionSystem, IEcsRunSystem
 {
-    private EcsComponentRef<SupportVisionComponent> _supportVisionComponentRef = default;
+    //private EcsComponentRef<SupportVisionComponent> _supportVisionComponentRef = default; //Programmer 2
 
 
     internal SupportVisionSystem(ECSmanager eCSmanager, SupportManager supportManager) : base(eCSmanager, supportManager)
@@ -15,29 +15,29 @@ public partial class SupportVisionSystem : CellReductionSystem, IEcsRunSystem
 
     public void Run()
     {
-        //_supportVisionComponentRef.Unref().Unpack(out SupportVisionTypes supportVisionType, out bool isActive);
+        _supportVisionComponentRef.Unref().Unpack(out SupportVisionTypes supportVisionType, out bool isActive);
 
-        //switch (supportVisionType)
-        //{
-        //    case SupportVisionTypes.SelectorVision:
-        //        ActiveSelectorVision(isActive);
-        //        break;
+        switch (supportVisionType)
+        {
+            case SupportVisionTypes.SelectorVision:
+                ActiveSelectorVision(isActive);
+                break;
 
-        //    case SupportVisionTypes.SpawnVision:
-        //        ActiveSpawnVision(isActive);
-        //        break;
+            case SupportVisionTypes.SpawnVision:
+                ActiveSpawnVision(isActive);
+                break;
 
-        //    case SupportVisionTypes.WayOfUnitVision:
-        //        ActiveWayOfUnitVision(isActive);
-        //        break;
+            case SupportVisionTypes.WayOfUnitVision:
+                ActiveWayOfUnitVision(isActive);
+                break;
 
-        //    case SupportVisionTypes.EnemyVision:
-        //        ActiveEnemyVision(isActive);
-        //        break;
+            case SupportVisionTypes.EnemyVision:
+                ActiveEnemyVision(isActive);
+                break;
 
-        //    default:
-        //        break;
-        //}
+            default:
+                break;
+        }
     }
 
     private void ActiveSelectorVision(in bool isActive)
