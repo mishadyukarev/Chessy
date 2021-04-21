38dca85fc25666ae8b4a322fabc52c5d22d32b65
Misha Dyukarev <misha21022001@mail.ru>
2021-04-21 15:52:36 +0300

    Redo GetterCellSystem and Component

--- Assets/Scripts/ECS/ECSgeneral/Systems/Else/GetterCellSystem.cs
+++ Assets/Scripts/ECS/ECSgeneral/Systems/Else/GetterCellSystem.cs
@@ -1,4 +1,49 @@
 ﻿using Leopotam.Ecs;
+using UnityEngine;
+
+
+public struct GetterCellComponent
+{
+    private RaycastHit2D _raycastHit2dIN;
+
+    private int[] _xyCurrentCellOUT;
+    private bool _isReceivedOUT;
+
+    private SystemsGeneralManager _systemsGeneralManager;
+
+    public GetterCellComponent(StartValuesConfig nameValueManager, SystemsGeneralManager systemsGeneralManager)
+    {
+        _raycastHit2dIN = default;
+        _xyCurrentCellOUT = new int[nameValueManager.XY_FOR_ARRAY];
+        _isReceivedOUT = default;
+
+        _systemsGeneralManager = systemsGeneralManager;
+    }
+
+
+    public bool TryGetXYCurrentCell(RaycastHit2D raycastHit2D, out int[] xyCurrentCell)
+    {
+        _raycastHit2dIN = raycastHit2D;
+
+        _systemsGeneralManager.InvokeRunSystem(SystemGeneralTypes.Else, nameof(GetterCellSystem));
+
+        xyCurrentCell = _xyCurrentCellOUT;
+        return _isReceivedOUT;
+    }
+
+    internal void Unpack(out RaycastHit2D raycastHit2dIN)
+    {
+        raycastHit2dIN = _raycastHit2dIN;
+    }
+
+    internal void Pack(int[] xyCurrentCellOUT, bool isReceivedOUT)
+    {
+        _xyCurrentCellOUT = xyCurrentCellOUT;
+        _isReceivedOUT = isReceivedOUT;
+    }
+}
+
+
 
 public sealed class GetterCellSystem : CellReductionSystem, IEcsRunSystem
 {
@@ -11,29 +56,25 @@ public sealed class GetterCellSystem : CellReductionSystem, IEcsRunSystem
 
     public void Run()
     {
-        if (_getterCellComponentRef.Unref().RaycastHit2dIN != default)
+        _getterCellComponentRef.Unref().Unpack(out RaycastHit2D raycastHit2dIN);
+
+        if (raycastHit2dIN != default)
         {
             for (int x = 0; x < Xcount; x++)
             {
                 for (int y = 0; y < Ycount; y++)
                 {
-                    int one = CellComponent((sbyte)x, (sbyte)y).InstanceIDcell;
-                    int two = _getterCellComponentRef.Unref().RaycastHit2dIN.transform.gameObject.GetInstanceID();
+                    int one = CellComponent(x, y).InstanceIDcell;
+                    int two = raycastHit2dIN.transform.gameObject.GetInstanceID();
 
                     if (one == two)
                     {
-                        _getterCellComponentRef.Unref().XYcurrentCellOUT[0] = (sbyte)x;
-                        _getterCellComponentRef.Unref().XYcurrentCellOUT[1] = (sbyte)y;
-
-                        _getterCellComponentRef.Unref().SetIsReceiveOUT(true);
-                        break;
+                        _getterCellComponentRef.Unref().Pack(new int[] { x, y }, true);
+                        return;
                     }
-
-                    else _getterCellComponentRef.Unref().SetIsReceiveOUT(false);
                 }
-
-                if (_getterCellComponentRef.Unref().IsReceivedOUT) break;
             }
+            _getterCellComponentRef.Unref().Pack(new int[] { -1, -1 }, false);
         }
     }
 }
