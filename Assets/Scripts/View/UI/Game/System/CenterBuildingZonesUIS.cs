dc17a2052e573390dd9ea8872bf69cabdf062214
mishadyukarev <mishadyukarev64@gmail.com>
2022-04-10 11:23:29 +0300

    Base

--- Assets/Scripts/View/UI/Game/System/CenterBuildingZonesUIS.cs
+++ Assets/Scripts/View/UI/Game/System/CenterBuildingZonesUIS.cs
@@ -1,4 +1,6 @@
 ﻿using Chessy.Game.Values;
+using Chessy.Game.View.UI;
+using Chessy.Game.View.UI.Values;
 
 namespace Chessy.Game
 {
@@ -17,19 +19,19 @@ namespace Chessy.Game
             eUI.CenterEs.SmelterE.Zone.SetActive(e.SelectedE.BuildingsC.Is(BuildingTypes.Smelter));
 
 
-            eUI.CenterEs.MarketE.Text1C(MarketBuyTypes.FoodToWood).TextUI.text = (EconomyValues.FOR_BUY_FROM_MARKET_FOOD_TO_WOOD * 100).ToString();
-            eUI.CenterEs.MarketE.Text1C(MarketBuyTypes.WoodToFood).TextUI.text = (EconomyValues.FOR_BUY_FROM_MARKET_WOOD_TO_FOOD * 100).ToString();
+            eUI.CenterEs.MarketE.Text1C(MarketBuyTypes.FoodToWood).TextUI.text = (EconomyValues.FOR_BUY_FROM_MARKET_FOOD_TO_WOOD * Valuess.FOR_ROUND_UP_RESOURCES).ToString();
+            eUI.CenterEs.MarketE.Text1C(MarketBuyTypes.WoodToFood).TextUI.text = (EconomyValues.FOR_BUY_FROM_MARKET_WOOD_TO_FOOD * Valuess.FOR_ROUND_UP_RESOURCES).ToString();
             eUI.CenterEs.MarketE.Text1C(MarketBuyTypes.GoldToFood).TextUI.text = EconomyValues.FOR_BUY_FROM_MARKET_GOLD_TO_FOOD.ToString();
             eUI.CenterEs.MarketE.Text1C(MarketBuyTypes.GoldToWood).TextUI.text = EconomyValues.FOR_BUY_FROM_MARKET_GOLD_TO_WOOD.ToString();
 
-            eUI.CenterEs.MarketE.Text2C(MarketBuyTypes.FoodToWood).TextUI.text = (EconomyValues.AFTER_BUY_FROM_MARKET_FOOD_TO_WOOD * 100).ToString();
-            eUI.CenterEs.MarketE.Text2C(MarketBuyTypes.WoodToFood).TextUI.text = (EconomyValues.AFTER_BUY_FROM_MARKET_WOOD_TO_FOOD * 100).ToString();
-            eUI.CenterEs.MarketE.Text2C(MarketBuyTypes.GoldToFood).TextUI.text = EconomyValues.AFTER_BUY_FROM_MARKET_GOLD_TO_FOOD.ToString();
-            eUI.CenterEs.MarketE.Text2C(MarketBuyTypes.GoldToWood).TextUI.text = EconomyValues.AFTER_BUY_FROM_MARKET_GOLD_TO_WOOD.ToString();
+            eUI.CenterEs.MarketE.Text2C(MarketBuyTypes.FoodToWood).TextUI.text = (EconomyValues.AFTER_BUY_FROM_MARKET_FOOD_TO_WOOD * Valuess.FOR_ROUND_UP_RESOURCES).ToString();
+            eUI.CenterEs.MarketE.Text2C(MarketBuyTypes.WoodToFood).TextUI.text = (EconomyValues.AFTER_BUY_FROM_MARKET_WOOD_TO_FOOD * Valuess.FOR_ROUND_UP_RESOURCES).ToString();
+            eUI.CenterEs.MarketE.Text2C(MarketBuyTypes.GoldToFood).TextUI.text = (EconomyValues.AFTER_BUY_FROM_MARKET_GOLD_TO_FOOD * Valuess.FOR_ROUND_UP_RESOURCES).ToString();
+            eUI.CenterEs.MarketE.Text2C(MarketBuyTypes.GoldToWood).TextUI.text = (EconomyValues.AFTER_BUY_FROM_MARKET_GOLD_TO_WOOD * Valuess.FOR_ROUND_UP_RESOURCES).ToString();
 
 
-            eUI.CenterEs.SmelterE.TextC(ResourceTypes.Wood).TextUI.text = (EconomyValues.WOOD_NEED_FOR_MELTING * 100).ToString();
-            eUI.CenterEs.SmelterE.TextC(ResourceTypes.Ore).TextUI.text = (EconomyValues.ORE_NEED_FOR_MELTING * 100).ToString();
+            eUI.CenterEs.SmelterE.TextC(ResourceTypes.Wood).TextUI.text = (EconomyValues.WOOD_NEED_FOR_MELTING * Valuess.FOR_ROUND_UP_RESOURCES).ToString();
+            eUI.CenterEs.SmelterE.TextC(ResourceTypes.Ore).TextUI.text = (EconomyValues.ORE_NEED_FOR_MELTING * Valuess.FOR_ROUND_UP_RESOURCES).ToString();
 
             eUI.CenterEs.SmelterE.TextC(ResourceTypes.Iron).TextUI.text = EconomyValues.IRON_AFTER_MELTING.ToString();
             eUI.CenterEs.SmelterE.TextC(ResourceTypes.Gold).TextUI.text = EconomyValues.GOLD_AFTER_MELTING.ToString();
---
+++ Assets/Scripts/View/UI/Game/Values/Valuess.cs
@@ -0,0 +1,8 @@
+﻿namespace Chessy.Game.View.UI.Values
+{
+    sealed class Valuess
+    {
+        internal const int FOR_ROUND_UP_RESOURCES = 100;
+
+    }
+}
