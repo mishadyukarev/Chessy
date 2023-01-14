f9c737c7d426c360aaa258430ad9fbf438c12d53
Misha <mishadyukarev64@gmail.com>
2023-01-14 09:48:20 +0300

    Added language

---
+++ Assets/Scripts/View/UI/Enums/TextUIType.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Chessy.View.UI.Values
+{
+    public enum TextUIType
+    {
+        None,
+
+        Settings,
+    }
+}
---
+++ Assets/Scripts/View/UI/Values/ForUITextValues.cs
@@ -0,0 +1,32 @@
+﻿using Chessy.Model;
+using System;
+
+namespace Chessy.View.UI.Values
+{
+    public static class ForUITextValues
+    {
+        public static string GetText(in LanguageTypes languageT, in TextUIType textUIT)
+        {
+            return languageT switch
+            {
+                LanguageTypes.None => throw new Exception(),
+                LanguageTypes.English => textUIT switch
+                {
+                    TextUIType.None => throw new Exception(),
+                    TextUIType.Settings => "Settings",
+                    _ => throw new Exception(),
+                },
+                LanguageTypes.Russian => textUIT switch
+                {
+                    TextUIType.None => throw new Exception(),
+                    TextUIType.Settings => "Настройки",
+                    _ => throw new Exception(),
+                },
+                LanguageTypes.Chinese => throw new Exception(),
+                LanguageTypes.Spanish => throw new Exception(),
+                LanguageTypes.End => throw new Exception(),
+                _ => throw new Exception(),
+            };
+        }
+    }
+}
